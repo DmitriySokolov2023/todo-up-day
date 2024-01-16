ab9fdfce7ada6fd555abf64d361bbd601541d6a7
sokolovofficial2023 <dev@tararoutray.com>
2024-01-16 22:45:36 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="w-full font-light text-2xl">
+        <div className='flex flex-col items-center'>
+            <div className='border px-4 w-1/2 flex justify-center my-2 rounded'>Задачи на день</div>
+            <div className='border px-4 w-1/2 flex justify-center my-2 rounded'>Задачи на день</div>
+            <div className='border px-4 w-1/2 flex justify-center my-2 rounded'>Задачи на день</div>
+            <div className='border px-4 w-1/2 flex justify-center my-2 rounded'>Задачи на день</div>
+        </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/UI/button/MyButton.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from "./MyButton.module.css";
+
+const MyButton = () => {
+    return (
+        <div className={classes.btn}>
+            
+        </div>
+    );
+};
+
+export default MyButton;
